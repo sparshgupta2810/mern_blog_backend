b86280ae6ff2e41651021bcf4324ed3701f6d6bc
sparshgupta2810 <134164704+sparshgupta2810@users.noreply.github.com>
2024-07-04 16:47:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,12 @@ const app = express();
 
 app.use(express.json({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({
+  origin: (origin, callback) => {
+    callback(null, true);  // Allow all origins
+  },
+  credentials: true  // Allow credentials
+}));
 app.use(upload());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 app.use('/api/users', userRoutes);
